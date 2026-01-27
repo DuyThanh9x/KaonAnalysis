11997a6c4f689af018e1b27c1c2051cbb49c1b7a
Thanh <thanh@se58-58.jinr.ru>
2026-01-27 11:16:56 +0300

    Remove selection

--- reduceDst.cc
+++ reduceDst.cc
@@ -1,14 +1,37 @@
-BmnKalmanFilter* fKalman = new BmnKalmanFilter();
 FairRunAna* fRunAna = new FairRunAna();
+#define TIME_CUT 66.0           // ns - upper bound (negative)
+#define TIME_MIN -1766.0        // ns - minimum time  
+#define BIN_WIDTH 50.0          // ns - bin width
+#define NUM_BINS 34             // (1766 - 66) / 50 = 34 bins
+#define PILEUP_THRESHOLD 10     // hits per bin threshold for pileup
+
+bool isPileup(TTreeReaderArray<BmnTofHit> &TofHit) {
+        int nHits = TofHit.GetSize();
+ 	int maxHitsTOF = 0;
+        int tof_bins[NUM_BINS] = {0};
+        for (int i = 0; i < nHits; i++) {
+                BmnTofHit hit = TofHit.At(i);
+                double time = hit.GetTimeStamp();
+                if (time >= TIME_MIN && time <= -TIME_CUT) {
+                    int bin = (int)((time - TIME_MIN) / BIN_WIDTH);
+                    if (bin >= 0 && bin < NUM_BINS) {
+                        tof_bins[bin]++;
+                    }
+                }
+            }
+        for (int bin = 0; bin < NUM_BINS; bin++) {
+                if (tof_bins[bin] > maxHitsTOF) {
+                    maxHitsTOF = tof_bins[bin];
+                }
+            }
+        if (maxHitsTOF >= PILEUP_THRESHOLD) {
+                 return true;
+        } else return false;
+}
 
 void reduceData (int runPeriod, int runID, int evID, int partID)
 {
-	
-	//gInterpreter->GenerateDictionary("vector<TVector3>","vector;TVector3.h");
-	//auto clss=TClass::GetClass("vector<TVector3>");
-	//if (!clss || !clss->GetCollectionProxy()) Error("check","no proxy");
 	gRandom->SetSeed(0);
-       // put proper run number
         TString geoFileName = Form("current_geo_file_%d.root", UInt_t(gRandom->Integer(UINT32_MAX)));
         Int_t res_code = UniRun::ReadGeometryFile(runPeriod, runID, (char*)geoFileName.Data());
         TGeoManager::Import(geoFileName);
@@ -34,11 +57,10 @@ void reduceData (int runPeriod, int runID, int evID, int partID)
 	    <<">>>>>>>>>> Beam:   "<<beam<<endl;
 
 	TFile *input = TFile::Open(Form("mpd_run_Top_%d_ev%d_p%d.root",runID,evID,partID));
-
 	TTreeReader Intree("bmndata",input);
-	TTreeReaderValue<BmnTrigInfoDst> trigger(Intree,"BmnTrigInfo.");
 	TTreeReaderValue<CbmVertex> vertex(Intree,"MpdVertex.");
 	TTreeReaderArray<BmnGlobalTrack> track(Intree,"BmnGlobalTrack");
+	TTreeReaderArray<CbmStsTrack> stsvectr(Intree,"StsVector");
 	TTreeReaderArray<BmnTofHit> tof400Hit(Intree,"BmnTof400Hit");
 	TTreeReaderArray<BmnTofHit> tof700Hit(Intree,"BmnTof700Hit");
 
@@ -50,16 +72,22 @@ void reduceData (int runPeriod, int runID, int evID, int partID)
 	int nEvents = 0;
 	TVector3 PVertexPos(-100, -100, -100);
 
-	//gSystem->Load("AutoDict_vector_TVector3__cxx.so");
 	std::vector<double> TrackStartPointX, TrackStartPointY, TrackStartPointZ, TrackEndPointX, TrackEndPointY, TrackEndPointZ;
 	TrackStartPointX.clear(); TrackStartPointY.clear(); TrackStartPointZ.clear(); TrackEndPointX.clear(); TrackEndPointY.clear(); TrackEndPointZ.clear();
 
 	double PVChi2 = -1;
-
+	bool isPileup400 = false, isPileup700  = false;
 	vector<double> TrackChi2, TrackLength, TrackPq, TrackPx, TrackPy, TrackPz, TrackTof400Mq2, TrackTof700Mq2, Beta400, Beta700;
 	TrackChi2.clear(); TrackLength.clear(); TrackPq.clear(); TrackPx.clear(); TrackPy.clear(); TrackPz.clear();
 	TrackTof400Mq2.clear(); TrackTof700Mq2.clear(); Beta400.clear(); Beta700.clear();
-	
+	vector<int> TrackID;
+	TrackID.clear();
+
+	vector<double> MLtrack;
+	MLtrack.clear();
+	vector<int> StsVectrID;
+        StsVectrID.clear();
+
 	int PVNTracks = -1, PVNDF = -1;
 	
 	vector<int> TrackNHits, TrackNDF, TrackTof400HitID, TrackTof700HitID;
@@ -67,6 +95,10 @@ void reduceData (int runPeriod, int runID, int evID, int partID)
 	
 	vector<double> HitPosX400, HitPosY400, HitPosZ400;
 	HitPosX400.clear(); HitPosY400.clear(); HitPosZ400.clear();
+	
+	vector<int> Hit400PlaneNumber, Hit400StripNumber;
+	Hit400PlaneNumber.clear();Hit400StripNumber.clear();
+
 	vector<double> HitTimeOfFlight400;
 	HitTimeOfFlight400.clear();
 	vector<double> HitLength400;
@@ -74,7 +106,9 @@ void reduceData (int runPeriod, int runID, int evID, int partID)
 
 	vector<double> HitPosX700, HitPosY700, HitPosZ700;
         HitPosX700.clear(); HitPosY700.clear(); HitPosZ700.clear();
-        vector<double> HitTimeOfFlight700;
+        vector<int> Hit700PlaneNumber, Hit700StripNumber;
+        Hit700PlaneNumber.clear();Hit700StripNumber.clear();
+	vector<double> HitTimeOfFlight700;
         HitTimeOfFlight700.clear();
 	vector<double> HitLength700;
 	HitLength700.clear();
@@ -92,6 +126,7 @@ void reduceData (int runPeriod, int runID, int evID, int partID)
 	NewTree->Branch("TrackEndPointY",&TrackEndPointY);
 	NewTree->Branch("TrackEndPointZ",&TrackEndPointZ);
 
+	NewTree->Branch("TrackID",&TrackID);
 	NewTree->Branch("TrackTof400HitID",&TrackTof400HitID);
 	NewTree->Branch("TrackTof700HitID",&TrackTof700HitID);
 	NewTree->Branch("TrackPq",&TrackPq);
@@ -107,28 +142,32 @@ void reduceData (int runPeriod, int runID, int evID, int partID)
 	NewTree->Branch("TrackChi2",&TrackChi2);
 	NewTree->Branch("TrackLength",&TrackLength);
 
+	NewTree->Branch("TMVATrack",&MLtrack);
+
 	NewTree->Branch("HitPosX400",&HitPosX400);
 	NewTree->Branch("HitPosY400",&HitPosY400);
 	NewTree->Branch("HitPosZ400",&HitPosZ400);
 	NewTree->Branch("HitTimeOfFlight400",&HitTimeOfFlight400);
 	NewTree->Branch("HitLength400",&HitLength400);
+	NewTree->Branch("Hit400PlaneNumber",&Hit400PlaneNumber);
+	NewTree->Branch("Hit400StripNumber",&Hit400StripNumber);
+	NewTree->Branch("isPileup400",&isPileup400);
 
 	NewTree->Branch("HitPosX700",&HitPosX700);
 	NewTree->Branch("HitPosY700",&HitPosY700);
 	NewTree->Branch("HitPosZ700",&HitPosZ700);
         NewTree->Branch("HitTimeOfFlight700",&HitTimeOfFlight700);
 	NewTree->Branch("HitLength700",&HitLength700);
+	NewTree->Branch("Hit700PlaneNumber",&Hit700PlaneNumber);
+        NewTree->Branch("Hit700StripNumber",&Hit700StripNumber);
+	NewTree->Branch("isPileup700",&isPileup700);
 	
 	int EventID = 0;
 	while (Intree.Next()) {
 		if (EventID % 1000 == 0)
 			cout<<">>>>>>>>>>>>>>>>Event ID: "<<EventID<<endl;
-		//Intree.GetEntry(EventID);
 		EventID++;
-		//if (!(trigger->IsTriggerBitTrueAR(7)))
-			//continue;
-	
-		if(vertex->GetNTracks() < 4)
+		/*if(vertex->GetNTracks() < 2)
 			continue;
 
 		if (vertex->GetZ() < -0.5 || vertex->GetZ() > +0.5)
@@ -139,8 +178,9 @@ void reduceData (int runPeriod, int runID, int evID, int partID)
         	Float_t radiusOfTarget = 1.2;
 
         	if (TMath::Sqrt(TMath::Sq(vertex->GetX() - centerOfTargetX) + TMath::Sq(vertex->GetY() - centerOfTargetY)) > radiusOfTarget)
-            		continue;
-
+            		continue;*/
+		if (isPileup(tof400Hit)) isPileup400 = true; else isPileup400 = false;
+		if (isPileup(tof700Hit)) isPileup700 = true; else isPileup700  = false; //continue;
 		PVertexPos.SetX(vertex->GetX());
 		PVertexPos.SetY(vertex->GetY());
 		PVertexPos.SetZ(vertex->GetZ());
@@ -150,12 +190,12 @@ void reduceData (int runPeriod, int runID, int evID, int partID)
 		for (BmnGlobalTrack& gl : track) {
 			//int n = track.GetEntriesFast();
 			
-			Int_t pdg = (gl.GetParamFirst()->GetQp() > 0) ? 2212 : -211;
+			/*Int_t pdg = (gl.GetParamFirst()->GetQp() > 0) ? 2212 : -211;
         		if (fKalman->TGeoTrackPropagate(gl.GetParamFirst(), vertex->GetZ(), pdg, nullptr, nullptr, kTRUE) == kBMNERROR)
                			 continue;
             		Float_t dtrack = TMath::Sqrt(TMath::Sq(gl.GetParamFirst()->GetX() - vertex->GetX()) + TMath::Sq(gl.GetParamFirst()->GetY() - vertex->GetY()));
             		if (dtrack > 1.0)
-                		continue;
+                		continue;*/
 
 			TrackStartPointX.push_back(gl.GetParamFirst()->GetX());
 			TrackStartPointY.push_back(gl.GetParamFirst()->GetY());
@@ -164,6 +204,9 @@ void reduceData (int runPeriod, int runID, int evID, int partID)
 			TrackEndPointY.push_back(gl.GetParamLast()->GetY());
 			TrackEndPointZ.push_back(gl.GetParamLast()->GetZ());
 
+			TrackID.push_back(gl.GetGemTrackIndex());
+			CbmStsTrack &ststrack = stsvectr.At(gl.GetGemTrackIndex());
+			MLtrack.push_back(ststrack.GetB());
 			TrackTof400HitID.push_back(gl.GetTof1HitIndex());
 			TrackTof700HitID.push_back(gl.GetTof2HitIndex());
 			TrackPq.push_back(gl.GetP());
@@ -179,23 +222,50 @@ void reduceData (int runPeriod, int runID, int evID, int partID)
 			TrackChi2.push_back(gl.GetChi2());
 			TrackLength.push_back(gl.GetLength());
 			TrackNDF.push_back(gl.GetNDF());
-		}
 
-		for (BmnTofHit& hit : tof400Hit) {
-			HitPosX400.push_back(hit.GetX());
-			HitPosY400.push_back(hit.GetY());
-			HitPosZ400.push_back(hit.GetZ());
-			HitTimeOfFlight400.push_back(hit.GetTimeStamp());
-			HitLength400.push_back(hit.GetLength());
-		}
+			if (gl.GetTof1HitIndex() != -1) {
+				BmnTofHit &hit = tof400Hit.At(gl.GetTof1HitIndex());
+				HitPosX400.push_back(hit.GetX());
+				HitPosY400.push_back(hit.GetY());
+				HitPosZ400.push_back(hit.GetZ());
+				HitTimeOfFlight400.push_back(hit.GetTimeStamp());
+				HitLength400.push_back(hit.GetLength());
+				Hit400PlaneNumber.push_back(hit.GetModule());
+				Hit400StripNumber.push_back(hit.GetStation());
+				}
+				else {
+					HitPosX400.push_back(-10000);
+					HitPosY400.push_back(-10000);
+					HitPosZ400.push_back(-10000);
+					HitLength400.push_back(-10000);
+					HitTimeOfFlight400.push_back(-10000);
+					Hit400PlaneNumber.push_back(-1);
+					Hit400StripNumber.push_back(-1);
+			
+				}
+			if (gl.GetTof2HitIndex() != -1) {
+				BmnTofHit &hit2 = tof700Hit.At(gl.GetTof2HitIndex());
+					HitPosX700.push_back(hit2.GetX());
+					HitPosY700.push_back(hit2.GetY());
+					HitPosZ700.push_back(hit2.GetZ());
+                        		HitTimeOfFlight700.push_back(hit2.GetTimeStamp());
+					HitLength700.push_back(hit2.GetLength());
+               
+					Hit700PlaneNumber.push_back(hit2.GetModule());
+                        		Hit700StripNumber.push_back(hit2.GetStation());
+				}
+				else {
+                                        HitPosX700.push_back(-10000);
+                                        HitPosY700.push_back(-10000);
+                                        HitPosZ700.push_back(-10000);
+					HitLength700.push_back(-10000);
+                                        HitTimeOfFlight700.push_back(-10000);
+                                        Hit700PlaneNumber.push_back(-1);
+                                        Hit700StripNumber.push_back(-1);
 
-		for (BmnTofHit& hit2 : tof700Hit) {
-                        HitPosX700.push_back(hit2.GetX());
-			HitPosY700.push_back(hit2.GetY());
-			HitPosZ700.push_back(hit2.GetZ());
-                        HitTimeOfFlight700.push_back(hit2.GetTimeStamp());
-			HitLength700.push_back(hit2.GetLength());
-                }
+                        	}
+
+		}
 
 		NewTree->Fill();
 		nEvents++;
@@ -204,7 +274,12 @@ void reduceData (int runPeriod, int runID, int evID, int partID)
 		TrackChi2.clear(); TrackLength.clear(); TrackPq.clear(); TrackTof400Mq2.clear(); TrackTof700Mq2.clear(); Beta400.clear(); Beta700.clear();
         	TrackPx.clear(); TrackPy.clear(); TrackPz.clear();
 		TrackNHits.clear(); TrackNDF.clear(); TrackTof400HitID.clear(); TrackTof700HitID.clear();
+
+		TrackID.clear();
 		
+		StsVectrID.clear();
+		MLtrack.clear();
+
 		HitPosX400.clear(); HitPosY400.clear(); HitPosZ400.clear();
 	        HitTimeOfFlight400.clear();
 		HitLength400.clear();
@@ -213,6 +288,8 @@ void reduceData (int runPeriod, int runID, int evID, int partID)
 	        HitTimeOfFlight700.clear();
 		HitLength700.clear();
 		
+		Hit400PlaneNumber.clear();Hit400StripNumber.clear();
+		Hit700PlaneNumber.clear();Hit700StripNumber.clear();
 		PVertexPos.SetXYZ(-100, -100, -100);
 	}
 	
